42214ad8968c7a8fc99741feebf495e60b248cc1
dazfz <dazhi891@gmail.com>
2023-09-06 22:57:12 -0300

    implement get by: id, name, type and hp

--- pokemon/serializers.py
+++ pokemon/serializers.py
@@ -2,12 +2,28 @@ from rest_framework import serializers
 from pokemon.models import Pokemon, PokemonTypes
 
 
-# TODO: Add a serializer for PokemonTypes, and use it as a nested serializer for PokemonSerializer
+class TypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PokemonTypes
+        fields = ("type",)
 
 
 class PokemonSerializer(serializers.ModelSerializer):
+    types = TypeSerializer(many=True)
 
     class Meta:
         model = Pokemon
-        fields = ('id', 'name_english', 'name_japanese', 'name_chinese', 'name_french', 'hp', 'attack', 'defense',
-                  'special_attack', 'special_defense', 'speed', )
+        fields = (
+            "id",
+            "name_english",
+            "name_japanese",
+            "name_chinese",
+            "name_french",
+            "hp",
+            "attack",
+            "defense",
+            "special_attack",
+            "special_defense",
+            "speed",
+            "types",
+        )
