b413083240803e2754a3bf4021a47a472fe3c81a
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2025-01-07 19:10:13 +0300

    pokemon_by_name endpoint added

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,7 +1,8 @@
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
-
+from django.db.models import Q
+import pokemon
 from pokemon.models import Pokemon, VALID_POKEMON_TYPES
 from pokemon.serializers import PokemonSerializer
 
@@ -24,8 +25,7 @@ def pokemon_by_id(request, id):
     Get Pokemon by ID
     """
     pokemon = Pokemon.objects.get(id=id)
-    serializer = PokemonSerializer(pokemon, )
-    # TODO: Implement Endpoint
+    serializer = PokemonSerializer(pokemon)
     return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
 
 
@@ -35,8 +35,15 @@ def pokemon_by_name(request, name):
     """
     Get Pokemon by name
     """
-    # TODO: Implement Endpoint
-    return HttpResponse(status=501)
+    pokemon = Pokemon.objects.filter(
+            Q(name_english__icontains=name) |
+            Q(name_japanese__icontains=name) |
+            Q(name_chinese__icontains=name) |
+            Q(name_french__icontains=name)
+        
+    )
+    serializer = PokemonSerializer(pokemon, many=True)
+    return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
 
 
 @csrf_exempt
