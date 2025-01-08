73add50698b9dfa970946fc9bb97c759a3c3fa0a
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2025-01-08 11:51:39 +0300

    added pokemon type serializer

--- pokemon/serializers.py
+++ pokemon/serializers.py
@@ -2,12 +2,15 @@ from rest_framework import serializers
 from pokemon.models import Pokemon, PokemonTypes
 
 
-# TODO: Add a serializer for PokemonTypes, and use it as a nested serializer for PokemonSerializer
 
-
+class PokemonTypesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PokemonTypes
+        fields = ('type',) 
+        
 class PokemonSerializer(serializers.ModelSerializer):
-
+    types = PokemonTypesSerializer(many=True,read_only=True)
     class Meta:
         model = Pokemon
         fields = ('id', 'name_english', 'name_japanese', 'name_chinese', 'name_french', 'hp', 'attack', 'defense',
-                  'special_attack', 'special_defense', 'speed', )
+                  'special_attack', 'special_defense', 'speed', 'types',)
