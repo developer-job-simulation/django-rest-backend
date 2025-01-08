c8daa6c83d39eb338a2a13db9dda2e8291b250d7
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2025-01-08 12:06:08 +0300

    handle error for pokemon_by_hp endpoint

--- pokemon/views.py
+++ pokemon/views.py
@@ -16,7 +16,6 @@ def pokemon_list(request):
     serializer = PokemonSerializer(pokemon, many=True)
     return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
 
-
 @csrf_exempt
 @api_view(['GET'])
 def pokemon_by_id(request, id):
@@ -49,7 +48,6 @@ def pokemon_by_type(request, pokemon_type):
     """
     Get Pokemon by type
     """
-    pokemon_types = PokemonTypes.objects.filter(type=pokemon_type)
     pokemon_types = PokemonTypes.objects.filter(type__icontains=pokemon_type)
     pokemons = [ pokemon_type.pokemon for pokemon_type in pokemon_types]
     if pokemons == []:
@@ -62,20 +60,27 @@ def pokemon_by_hp(request):
     """
     Get Pokemon by HP
     """
-    # TODO: Implement Endpoint
-    return HttpResponse(status=501)
     params = request.GET
     hp_lt = request.GET.get("lt")
     hp_gt = request.GET.get("gt")
     query = Q()
+
+    for key in params:
+        if key not in ["gt","gte","lt","lte"]:
+            return JsonResponse({"error":'Invalid Operator. Must be one of ["gt","gte","lt","lte"]'})
+
     if hp_gt:
         query &= Q(hp__gt = hp_gt)
 
     if hp_lt:
         query &= Q(hp__lt = hp_lt)
+
     if hp_gt and hp_lt:
         if hp_gt > hp_lt:
             return JsonResponse({"error":"Not found"},status=404)
+
     pokemons = Pokemon.objects.filter(query)
     serializer = PokemonSerializer(pokemons,many=True)
+
     return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
+
